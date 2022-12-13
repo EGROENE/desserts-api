3a1842dd923af2e1b772ba022da21e77d5f122c8
Ethan <ethangroene@gmail.com>
2022-12-12 20:53:45 -0500

    Func to del certain dessert from homepage

--- js/main.js
+++ js/main.js
@@ -24,7 +24,8 @@ async function popSections() {
     const dessertsFavs = document.getElementById('desserts-container-favs');
     for (let dessert of mainArray) {
         // This consolidation can be seen around min 17 of Andrey's video
-        [dessertsHomepage, dessertsFavs].map((section) => {
+        //[dessertsHomepage, dessertsFavs].map((section) => {
+        [dessertsHomepage].map((section) => {
             // Params are equal to one of the arrays, depending on whether the section is dessertsHomepage or not
             const params = section === dessertsHomepage
             ? ['heart', 'Add to Favorites']
@@ -32,7 +33,7 @@ async function popSections() {
             section.innerHTML += 
             "<div class='dessert'>"
                 + "<div class='dessert-img-container'>"
-                + "<i class='fas fa-" + params[0] + "'title='" + params[1] + "'></i>"
+                + "<button class='fav-btn'><i class='fas fa-" + params[0] + "'title='" + params[1] + "'></i></button>"
                 + "<img src='" + dessert.photoUrl + "'>"
                 + "</div>"
                 + "<header>" + dessert.name + "</header>"
@@ -44,7 +45,34 @@ async function popSections() {
     console.log(dessertsHomepage);
     console.log(dessertsFavs);
 }
-popSections();
+//popSections();
+
+// Function to del from homepage, add to favs:
+/* const favBtns = document.querySelectorAll('.fav-btn');
+for (let btn of favBtns) {
+    console.log('hi')
+    let btnIndex = favBtns.indexOf(btn);
+    btn.addEventListener('click', function() {
+        let dessertsArray = btn.parentElement.parentElement.parentElement;
+        dessertsArray.removeChild(dessertsArray.children[btnIndex]);
+    })
+} */
+async function toFavs() {
+    await popSections();
+    let favBtns = document.querySelectorAll('.fav-btn');
+    favBtns = Array.from(favBtns);
+    for (let btn of favBtns) {
+        console.log('hi')
+        let btnIndex = favBtns.indexOf(btn);
+        btn.addEventListener('click', function() {
+            let dessertsArray = btn.parentElement.parentElement.parentElement;
+            let dessert = btn.parentElement.parentElement
+            // particular dessert has to be removed:
+            dessertsArray.removeChild(dessert);
+        })
+    }
+}
+toFavs();
 
 // FAV MODAL JS
 const modalOpen = '[data-open]';
